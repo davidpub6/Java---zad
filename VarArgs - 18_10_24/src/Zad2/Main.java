813145158b2aaa4a23688dcfd842cfb22fd66e4b
Davidpub6 <57505750+davidpub6@users.noreply.github.com>
2024-10-18 12:14:55 +0200

    Add files via upload

--- VarArgs - 18_10_24/src/Zad2/Main.java
+++ VarArgs - 18_10_24/src/Zad2/Main.java
@@ -4,13 +4,15 @@ public class Main {
 
     public static String builder(int n, String ... word){
         String fin = "";
-        for(int i=0; i<= word.length; i+=n){
-            fin += word[i];
+        for(int i=0; i< word.length; i++){
+            if((i+1)%n != 0) {
+                fin += word[i];
+            }
         }
         return fin;
     }
 
     public static void main(String[] args) {
-        System.out.println(builder(2, "Ala ","nie ","ma ","psa i ","kota "));
+        System.out.println(builder(3, "Ala ","nie ","ma ","psa i ","kota ", "test "));
     }
 }
